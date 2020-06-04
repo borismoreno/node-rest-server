8cee2b7764b0537655c392c4c931df62c1528a4f
Boris Moreno <boris_marco_moreno@hotmail.com>
2020-06-03 22:36:05 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin_user:qDbKZsXJ0kCUuEpr@cluster0-aps5b.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
